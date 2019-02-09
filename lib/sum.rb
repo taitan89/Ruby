8310206b4ca2102d4a3118462c5c36f66b43f9c4
taitan89 <edo.inari.6954@gmail.com>
2019-02-10 03:28:29 +0900

    sum.rb のテスト

---
+++ lib/sum.rb
@@ -0,0 +1,8 @@
+module Sum
+  def self.hello
+    return "Hello"
+  end
+  def self.add(a,b)
+    return a+b
+  end
+end
---
+++ spec/lib/sum_spec.rb
@@ -0,0 +1,23 @@
+require "spec_helper"
+require "sum"
+
+RSpec.describe Sum do
+  describe ".hello" do
+    it "'Hello'と返す" do
+      expect(Sum.hello).to eq("Hello")
+    end
+  end
+  describe ".add(a,b)" do
+    context "a=3,b=5の時" do
+      it "8を返す" do
+        result = Sum.add(3,5)
+        expect(result).to eq(8)
+      end
+    end
+    context "a=10,b=100の時" do
+      it "110を返す" do
+        expect(Sum.add(10,100)).to eq(110)
+      end
+    end
+  end
+end
